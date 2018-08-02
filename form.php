66fa2b614848fc28dfc83a109b35923b7e062e1c
Vladij <vladij01@gmail.com>
2018-08-02 15:42:13 +0300

    Форма html

--- form.php
+++ form.php
@@ -3,5 +3,5 @@
     <input type="tel" name="phone">
     <input type="email" name="email">
     <input type="file" name="files">
-    <input type="submit">
+    <input type="submit" name="submit">
 </form>
