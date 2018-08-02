f7d7a5d9dadf887c88bbf34aa5c7a57d1554e2ae
Vladij <vladij01@gmail.com>
2018-08-02 15:01:24 +0300

    <form action="send.php" method="POST">     <input type="text" name="name" />     <input type="tel" name="phone">     <input type="email" name="email">     <input type="file" name="files">     <input type="submit"> </form>

---
+++ form.php
@@ -0,0 +1,7 @@
+<form action="send.php" method="POST">
+    <input type="text" name="name" />
+    <input type="tel" name="phone">
+    <input type="email" name="email">
+    <input type="file" name="files">
+    <input type="submit">
+</form>
--- send.php
+++ send.php
@@ -14,25 +14,28 @@ $subject = 'Тема письма';
 //пишем условия для получения данных с формы
 
 if($_POST['name']){  //если поле 'name' заполнено
-  message = "<p> Имя: " . $_POST['name'] . "</p>"; //если выполнено, то формируем строку
+  $message = "<p> Имя: " . $_POST['name'] . "</p>"; //если выполнено, то формируем строку и записываем в переменную
 }
-if($_POST['name']){  //если поле 'name' заполнено
-  message = "<p> Имя: " . $_POST['name'] . "</p>"; //если выполнено, то формируем строку
+if($_POST['phone']){  //если поле 'phone' заполнено
+  $message .= "<p> Телефон: " . $_POST['phone'] . "</p>";
+}
+if($_POST['email']){  //если поле 'phone' заполнено
+    $message .= "<p> E-mail: " . $_POST['email'] . "</p>";
 }
 //конец описания
 
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
-$mail->From = 'info@sudexpa.ru';      // от кого
-$mail->FromName = 'Call';   // от кого
-$mail->AddAddress('daniil_kim_7@mail.ru');   // кому - адрес, Имя
+$mail->From = 'info@info.ru';      // от кого
+$mail->FromName = 'Name from';   // от кого
+$mail->AddAddress('email@email.com');   // кому - адрес, Имя
 $mail->IsHTML(true);        // выставляем формат письма HTML
 $mail->Subject = $subject;  // тема письма
 $mail->Body = $message;
-if(isset($_FILES['file_downloade'])) {
-    if($_FILES['file_downloade']['error'] == 0){
-        $mail->AddAttachment($_FILES['file_downloade']['tmp_name'],
-                             $_FILES['file_downloade']['name']);
+if(isset($_FILES['file_downloade'])) { //добавляем возможность отправки файла на почту через input type="file" с name="files"
+    if($_FILES['files']['error'] == 0){
+        $mail->AddAttachment($_FILES['files']['tmp_name'],
+                             $_FILES['files']['name']);
     }
 }
 
@@ -45,5 +48,5 @@ if(!$mail->send()) {
 
 ?>
 <script>
-     window.location = 'http://.../thank-you';
+     window.location = 'http://.../thank-you'; //мгновенная переадресация на другую страницу
 </script>
